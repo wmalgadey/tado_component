08d2644cab73b1201e16c198076131ab1fa6de28
Wolfgang Malgadey <w.malgadey@cp-pro.de>
2017-06-13 15:27:10 +0200

    Changed Pytado version

--- custom_components/tado_v1.py
+++ custom_components/tado_v1.py
@@ -15,7 +15,7 @@ _LOGGER = logging.getLogger(__name__)
 
 DOMAIN = 'tado_v1'
 
-REQUIREMENTS = ['https://github.com/wmalgadey/PyTado/archive/0.1.10.zip#PyTado==0.1.10']
+REQUIREMENTS = ['https://github.com/wmalgadey/PyTado/archive/0.2.1.zip#PyTado==0.2.1']
 
 TADO_V1_COMPONENTS = [
     'sensor', 'climate'
